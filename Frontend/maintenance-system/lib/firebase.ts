6b61dc7ae66f12536fe01d39a94ca927b43162f2
Muhle Mabunda <134883431+Shakira2022@users.noreply.github.com>
2025-10-26 13:50:52 +0200

    Update firebase.ts

--- Frontend/maintenance-system/lib/firebase.ts
+++ Frontend/maintenance-system/lib/firebase.ts
@@ -7,7 +7,6 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-/*
 const firebaseConfig = {
   apiKey: "AIzaSyAXsXO7r2aFVhzU0RCpYvzX_czW0zPDujs",
   authDomain: "reshelp-ba48b.firebaseapp.com",
@@ -18,7 +17,8 @@ const firebaseConfig = {
   appId: "1:109807635599:web:48f12d6608270a03f074fa",
   measurementId: "G-D2R1ME3KTC"
 };
-*/
+
+/*
 
 const firebaseConfig = {
   apiKey: "AIzaSyBMDut7COBOMNa9edbfdO1uKo7UMBvHueQ",
@@ -29,6 +29,8 @@ const firebaseConfig = {
   appId: "1:132832428:web:02da36805e7e1442acd309",
   measurementId: "G-10WEP3Q1Y8"
 };
+
+*/
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
